195f40be97a944c5e510bd6334ad98af04da4638
Joshua Mull <jmull@MHL2020.com>
2025-07-24 17:03:28 -0700

    Milestone table finished

---
+++ Kratos_tables/loanmilestone_table.sql
@@ -0,0 +1,133 @@
+
+CREATE TABLE dbo.LoanMilestone (
+    LoanNumber            VARCHAR(50),
+
+    -- Milestone Dates + Durations
+    StartedDate    DATETIME,
+    DaysAtStarted  INT,
+
+    InitialDisclosuresDate   DATETIME,
+    DaysAtInitialDisclosures INT,
+
+    ProcessingDate   DATETIME,
+    DaysAtProcessing INT,
+
+    SubmittalDate     DATETIME,
+    DaysAtSubmittal   INT,
+
+    CondApprovalDate     DATETIME,
+    DaysAtCondApproval   INT,
+
+    ResubmittalDate    DATETIME,
+    DaysAtResubmittal  INT,
+
+    ShippingDate     DATETIME,
+    DaysAtShipping   INT,
+
+    ApprovalDate      DATETIME,
+    DaysAtApproval    INT,
+
+    ClearToCloseDate    DATETIME,
+    DaysAtClearToClose  INT,
+
+    DocsOrderedDate    DATETIME,
+    DaysAtDocsOrdered  INT,
+
+    DocsSigningDate     DATETIME,
+    DaysAtDocsSigning   INT,
+
+    FundingDate    DATETIME,
+    DaysAtFunding  INT,
+
+    FinalDocsDate    DATETIME,
+    DaysAtFinalDocs  INT,
+
+    CompletionDate    DATETIME,
+    DaysAtCompletion  INT,
+
+    -- Final column
+    LastModifiedUtc  DATETIME
+);
+
+
+
+-- Pivot 1: Completion Dates
+WITH DatePivot AS (
+    SELECT *
+    FROM (
+        SELECT 
+            l.loannumber, 
+            Stage = REPLACE(mtl.Stage, ' ', ''),  -- Remove spaces
+            DateComplete = CASE WHEN mtl.DoneIndicator > 0 THEN mtl.DateUtc ELSE NULL END
+        FROM elliedb.milestonelog mtl 
+        JOIN elliedb.Loan l ON mtl.EncompassId = l.EncompassId
+    ) AS src
+    PIVOT (
+        MAX(DateComplete)
+        FOR Stage IN (
+            [Started], [InitialDisclosures], [Processing], [Submittal], [CondApproval],
+            [Resubmittal], [Shipping], [Approval], [ClearToClose], [DocsOrdered],
+            [DocsSigning], [Funding], [FinalDocs], [Completion]
+        )
+    ) AS pvt
+),
+
+-- Pivot 2: Durations
+DurationPivot AS (
+    SELECT *
+    FROM (
+        SELECT 
+            l.loannumber,
+            Stage = REPLACE(mtl.Stage, ' ', ''),
+            DaysSpent = CASE WHEN mtl.Duration < 0 THEN NULL ELSE mtl.Duration END
+        FROM elliedb.milestonelog mtl 
+        JOIN elliedb.Loan l ON mtl.EncompassId = l.EncompassId
+    ) AS src
+    PIVOT (
+        MAX(DaysSpent)
+        FOR Stage IN (
+            [Started], [InitialDisclosures], [Processing], [Submittal], [CondApproval],
+            [Resubmittal], [Shipping], [Approval], [ClearToClose], [DocsOrdered],
+            [DocsSigning], [Funding], [FinalDocs], [Completion]
+        )
+    ) AS pvt
+),
+
+-- Latest ModifiedUtc per loan
+LatestModified AS (
+    SELECT 
+        l.loannumber,
+        MAX(mtl.ModifiedUtc) AS LastModifiedUtc
+    FROM elliedb.milestonelog mtl
+    JOIN elliedb.Loan l ON mtl.EncompassId = l.EncompassId
+    GROUP BY l.loannumber
+)
+
+-- Final result
+SELECT 
+    d.loannumber,
+
+    -- StageDate and DaysAtStage pairs
+    d.Started               AS StartedDate,             dur.Started              AS DaysAtStarted,
+    d.InitialDisclosures    AS InitialDisclosuresDate,  dur.InitialDisclosures   AS DaysAtInitialDisclosures,
+    d.Processing            AS ProcessingDate,          dur.Processing           AS DaysAtProcessing,
+    d.Submittal             AS SubmittalDate,           dur.Submittal            AS DaysAtSubmittal,
+    d.CondApproval          AS CondApprovalDate,        dur.CondApproval         AS DaysAtCondApproval,
+    d.Resubmittal           AS ResubmittalDate,         dur.Resubmittal          AS DaysAtResubmittal,
+    d.Shipping              AS ShippingDate,            dur.Shipping             AS DaysAtShipping,
+    d.Approval              AS ApprovalDate,            dur.Approval             AS DaysAtApproval,
+    d.ClearToClose          AS ClearToCloseDate,        dur.ClearToClose         AS DaysAtClearToClose,
+    d.DocsOrdered           AS DocsOrderedDate,         dur.DocsOrdered          AS DaysAtDocsOrdered,
+    d.DocsSigning           AS DocsSigningDate,         dur.DocsSigning          AS DaysAtDocsSigning,
+    d.Funding               AS FundingDate,             dur.Funding              AS DaysAtFunding,
+    d.FinalDocs             AS FinalDocsDate,           dur.FinalDocs            AS DaysAtFinalDocs,
+    d.Completion            AS CompletionDate,          dur.Completion           AS DaysAtCompletion,
+
+    -- Final column: last modified
+    lm.LastModifiedUtc
+
+FROM DatePivot d
+JOIN DurationPivot dur ON d.loannumber = dur.loannumber
+JOIN LatestModified lm ON d.loannumber = lm.loannumber;
+
+
--- Kratos_tables/milestone_table.sql
+++ Kratos_tables/milestone_table.sql
@@ -1,24 +1,152 @@
-CREATE TABLE dbo.Milestone (
+CREATE TABLE dbo.LoanMilestone (
+    LoanNumber VARCHAR(50),
 
+    -- Milestone Dates + Durations
+    StartedDate DATETIME,
+    DaysAtStarted INT,
+
+    InitialDisclosuresDate DATETIME,
+    DaysAtInitialDisclosures INT,
+
+    ProcessingDate DATETIME,
+    DaysAtProcessing INT,
+
+    SubmittalDate DATETIME,
+    DaysAtSubmittal INT,
+
+    CondApprovalDate DATETIME,
+    DaysAtCondApproval INT,
+
+    ResubmittalDate DATETIME,
+    DaysAtResubmittal INT,
+
+    ShippingDate DATETIME,
+    DaysAtShipping INT,
+
+    ApprovalDate DATETIME,
+    DaysAtApproval INT,
+
+    ClearToCloseDate DATETIME,
+    DaysAtClearToClose INT,
+
+    DocsOrderedDate DATETIME,
+    DaysAtDocsOrdered INT,
+
+    DocsSigningDate DATETIME,
+    DaysAtDocsSigning INT,
+
+    FundingDate DATETIME,
+    DaysAtFunding INT,
+
+    FinalDocsDate DATETIME,
+    DaysAtFinalDocs INT,
+
+    CompletionDate DATETIME,
+    DaysAtCompletion INT,
+
+    -- Final column
+    LastModifiedUtc BIGINT
+);
+
+
+-- CTEs First
+WITH DatePivot AS (
+    SELECT *
+    FROM (
+        SELECT 
+            kl.loanID, 
+            Stage = REPLACE(mtl.Stage, ' ', ''),  
+            DateComplete = CASE WHEN mtl.DoneIndicator > 0 THEN mtl.DateUtc ELSE NULL END
+        FROM [WIN-T0FCRL091AK].Encompass.elliedb.milestonelog mtl 
+        JOIN [WIN-T0FCRL091AK].Encompass.elliedb.Loan l ON mtl.EncompassId = l.EncompassId
+        JOIN dbo.Loan kl ON l.LoanNumber = kl.LoanNum
+    ) AS src
+    PIVOT (
+        MAX(DateComplete)
+        FOR Stage IN (
+            [Started], [InitialDisclosures], [Processing], [Submittal], [CondApproval],
+            [Resubmittal], [Shipping], [Approval], [ClearToClose], [DocsOrdered],
+            [DocsSigning], [Funding], [FinalDocs], [Completion]
+        )
+    ) AS pvt
+),
+DurationPivot AS (
+    SELECT *
+    FROM (
+        SELECT 
+            kl.loanID,
+            Stage = REPLACE(mtl.Stage, ' ', ''),
+            DaysSpent = CASE WHEN mtl.Duration < 0 THEN NULL ELSE mtl.Duration END
+        FROM [WIN-T0FCRL091AK].Encompass.elliedb.milestonelog mtl 
+        JOIN [WIN-T0FCRL091AK].Encompass.elliedb.Loan l ON mtl.EncompassId = l.EncompassId
+        JOIN dbo.Loan kl ON l.LoanNumber = kl.LoanNum
+    ) AS src
+    PIVOT (
+        MAX(DaysSpent)
+        FOR Stage IN (
+            [Started], [InitialDisclosures], [Processing], [Submittal], [CondApproval],
+            [Resubmittal], [Shipping], [Approval], [ClearToClose], [DocsOrdered],
+            [DocsSigning], [Funding], [FinalDocs], [Completion]
+        )
+    ) AS pvt
+),
+LatestModified AS (
+    SELECT 
+        kl.loanID,
+        MAX(mtl.ModifiedUtc) AS LastModifiedUtc
+    FROM [WIN-T0FCRL091AK].Encompass.elliedb.milestonelog mtl
+    JOIN [WIN-T0FCRL091AK].Encompass.elliedb.Loan l ON mtl.EncompassId = l.EncompassId
+    JOIN dbo.Loan kl ON l.LoanNumber = kl.LoanNum
+    GROUP BY kl.loanID
 )
-SELECT ml.stage,
-    ml.tpoconnectstatus,
-    l.milestonefilestarteddate,
-    l.milestonesubmitteddate,
-    l.milestoneapproveddate,
-    l.milestonefundeddate,
-    --milestonelastcompleted = v.VirtualFieldValue,
-    l.milestonecurrentname
-FROM [WIN-T0FCRL091AK].Encompass.elliedb.Milestonelog ml
-LEFT JOIN [WIN-T0FCRL091AK].Encompass.elliedb.Loan l ON l.encompassid = ml.encompassid 
---LEFT JOIN [WIN-T0FCRL091AK].Encompass.elliedb.VirtualFields v ON v.encompassid = l.encompassid
-WHERE l.LoanNumber = 2503043904;
+
+-- Now do the insert
+INSERT INTO dbo.LoanMilestone (
+    LoanNumber,
+
+    StartedDate,            DaysAtStarted,
+    InitialDisclosuresDate, DaysAtInitialDisclosures,
+    ProcessingDate,         DaysAtProcessing,
+    SubmittalDate,          DaysAtSubmittal,
+    CondApprovalDate,       DaysAtCondApproval,
+    ResubmittalDate,        DaysAtResubmittal,
+    ShippingDate,           DaysAtShipping,
+    ApprovalDate,           DaysAtApproval,
+    ClearToCloseDate,       DaysAtClearToClose,
+    DocsOrderedDate,        DaysAtDocsOrdered,
+    DocsSigningDate,        DaysAtDocsSigning,
+    FundingDate,            DaysAtFunding,
+    FinalDocsDate,          DaysAtFinalDocs,
+    CompletionDate,         DaysAtCompletion,
+
+    LastModifiedUtc
+)
+SELECT 
+    d.loanID,
+
+    d.Started               AS StartedDate,              dur.Started              AS DaysAtStarted,
+    d.InitialDisclosures    AS InitialDisclosuresDate,   dur.InitialDisclosures   AS DaysAtInitialDisclosures,
+    d.Processing            AS ProcessingDate,           dur.Processing           AS DaysAtProcessing,
+    d.Submittal             AS SubmittalDate,            dur.Submittal            AS DaysAtSubmittal,
+    d.CondApproval          AS CondApprovalDate,         dur.CondApproval         AS DaysAtCondApproval,
+    d.Resubmittal           AS ResubmittalDate,          dur.Resubmittal          AS DaysAtResubmittal,
+    d.Shipping              AS ShippingDate,             dur.Shipping             AS DaysAtShipping,
+    d.Approval              AS ApprovalDate,             dur.Approval             AS DaysAtApproval,
+    d.ClearToClose          AS ClearToCloseDate,         dur.ClearToClose         AS DaysAtClearToClose,
+    d.DocsOrdered           AS DocsOrderedDate,          dur.DocsOrdered          AS DaysAtDocsOrdered,
+    d.DocsSigning           AS DocsSigningDate,          dur.DocsSigning          AS DaysAtDocsSigning,
+    d.Funding               AS FundingDate,              dur.Funding              AS DaysAtFunding,
+    d.FinalDocs             AS FinalDocsDate,            dur.FinalDocs            AS DaysAtFinalDocs,
+    d.Completion            AS CompletionDate,           dur.Completion           AS DaysAtCompletion,
+
+    lm.LastModifiedUtc
+
+FROM DatePivot d
+JOIN DurationPivot dur ON d.loanID = dur.loanID
+JOIN LatestModified lm ON d.loanID = lm.loanID;
 
 
-SELECT Loannumber, ProposedRealEstateTaxesAmount from [WIN-T0FCRL091AK].Encompass.elliedb.Loan WHERE ProposedRealEstateTaxesAmount ='n/a' 
 
 
-SELECT * --DISTINCT logrecordindex
-FROM [WIN-T0FCRL091AK].Encompass.elliedb.loan
-WHERE EncompassID = 'bb51f955-82bd-4636-b448-a0879003950c'
 
+--Select * from loanmilestone
